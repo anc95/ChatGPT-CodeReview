948b998cfcb2c134a16a8e29459cfcab3ecf55a5
anc95 <1481988258@qq.com>
2023-02-12 04:43:04 +0800

    update prompt

--- src/chat.ts
+++ src/chat.ts
@@ -7,19 +7,12 @@ const chatAPI = new ChatGPTAPI({
 export class Chat {
   private generatePrompt = (title: string, patch: string, extra?: string) => {
     return `
-    Please do code review for following diff
+    Please do a brief code review for following diff
 
     ${extra ? '-' + extra: ''}
     \n
     ${patch}
     \n\n
-    
-    As a code reviewer, your task is:
-    - Review the code changes (diffs) in the patch and provide feedback.
-    - If there are any bugs, highlight them.
-    - If the Pull request title is not good, please provide a better one.
-
-    Thank you in advance for your JOB
     `
   }
 
