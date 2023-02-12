0e672d898d1bbb57a4af7cb51437962e0e8bf5ed
anc95 <1481988258@qq.com>
2023-02-12 13:30:34 +0800

    update filter rule

--- middleware.js
+++ middleware.js
@@ -6,15 +6,15 @@ export default async function middleware(request) {
   const json = await request.json();
 
   if (!json) {
-    return;
+    return NextResponse.next();
   }
 
   if (json.action === "opened" && json.pull_request && json.pull_request.state === 'open') {
-    return;
+    return NextResponse.next();
   }
 
   if (json.action === 'created' && json.sender && json.sender.type === 'User' && json.comment && json.comment.body && json.comment.body.startsWith('@CR')) {
-    return
+    return NextResponse.next();
   }
 
   return Response.redirect('https://github.com/apps/cr-gpt');
