944b817b055494a8ac3ac5b5a303cd00e8021f3a
anc95 <1481988258@qq.com>
2023-02-12 11:45:31 +0800

    update config matcher

--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
 export const config = {
-  matcher: '/',
+  matcher: '/api/github/webhooks',
 };
 
 export default function middleware(request) {
+  console.log(JSON.stringify(request))
   return Response.redirect('https://github.com/apps/chatgpt-codereview-bot');
 }
