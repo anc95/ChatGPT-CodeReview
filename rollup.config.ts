f71f7535c9642004012e8960f8ed4f19c66d08fc
anc95 <1481988258@qq.com>
2023-02-12 19:16:54 +0800

    update middleware

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rollup'
 
 export default defineConfig([
   {
-    input: 'middleware.ts',
+    input: 'middleware.js',
     output: {
       file: 'dist/middleware.js',
       format: 'esm',
