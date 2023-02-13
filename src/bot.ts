a2b9291f7f87e4f36c6af46d2bc2e4939a0d98f7
chao.an <chao.an@grabtaxi.com>
2023-02-13 17:46:13 +0800

    add logs

--- src/bot.ts
+++ src/bot.ts
@@ -35,7 +35,7 @@ export const robot = (app: Probot) => {
     }
 
     try {
-      cr();
+      await cr();
     } catch (e) {
       console.error(e);
     }
@@ -60,7 +60,7 @@ export const robot = (app: Probot) => {
     }
 
     try {
-      cr();
+      await cr();
     } catch (e) {
       console.error(e);
     }
