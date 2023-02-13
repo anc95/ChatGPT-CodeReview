04f1daf09e6e6b47db54a99203b00b930a8677ec
chao.an <chao.an@grabtaxi.com>
2023-02-13 17:37:37 +0800

    add logs

--- rollup.config.ts
+++ rollup.config.ts
@@ -26,7 +26,7 @@ export default defineConfig([
     input: ['api/github/webhooks/index.ts'],
     output: {
       dir: 'dist/api/github/webhooks',
-      format: 'cjs',
+      format: 'esm',
     },
     plugins: [esbuild()],
   },
