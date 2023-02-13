ab435695165042ca00f92af59def7704bc7f7d86
chao.an <chao.an@grabtaxi.com>
2023-02-13 17:15:24 +0800

    add logs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,18 @@
 import esbuild from 'rollup-plugin-esbuild';
 import { defineConfig } from 'rollup';
-import commonjs from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig([
-  {
-    input: 'middleware.ts',
-    output: {
-      file: 'dist/middleware.js',
-      format: 'esm',
-      inlineDynamicImports: true,
-    },
-    plugins: [esbuild(), commonjs(), nodeResolve()],
-  },
+  // {
+  //   input: 'middleware.ts',
+  //   output: {
+  //     file: 'dist/middleware.js',
+  //     format: 'esm',
+  //     inlineDynamicImports: true,
+  //   },
+  //   plugins: [esbuild(), commonjs(), nodeResolve()],
+  // },
   {
     input: 'src/bot.ts',
     output: {
