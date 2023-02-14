2637a6f652b737f07212a42627758b1cd6a1bf29
anc95 <1481988258@qq.com>
2023-02-15 01:03:37 +0800

    update prompt

--- src/chat.ts
+++ src/chat.ts
@@ -9,9 +9,7 @@ export class Chat {
   }
 
   private generatePrompt = (patch: string) => {
-    return `
-    Bellow is the code patch, please help me do the code review\n
-
+    return `Bellow is the code patch, please help me do the code review\n
     ${patch}
     `;
   };
