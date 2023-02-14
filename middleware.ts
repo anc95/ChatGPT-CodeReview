7db9bdff8d57450b751d547ee44f8293cca55275
anc95 <1481988258@qq.com>
2023-02-14 23:39:35 +0800

    update deplot script

--- middleware.ts
+++ middleware.ts
@@ -7,13 +7,6 @@ export const config = {
 export default async function middleware(request: any) {
   let json;
 
-  setTimeout(() => {
-    console.log(1000);
-  });
-  setTimeout(() => {
-    console.log(1000);
-  }, 10000);
-
   try {
     console.log('enter');
     json = await request?.json?.();
@@ -24,27 +17,6 @@ export default async function middleware(request: any) {
   if (!json) {
     console.log('received is not a json');
     return rewrite(new URL('https://github.com/apps/cr-gpt'));
-  }
-
-  if (
-    json.action === 'opened' &&
-    json.pull_request &&
-    json.pull_request.state === 'open'
-  ) {
-    console.log('received an open pull_request');
-    return next();
-  }
-
-  if (
-    json.action === 'created' &&
-    json.sender &&
-    json.sender.type === 'User' &&
-    json.comment &&
-    json.comment.body &&
-    json.comment.body.startsWith('/cr.gpt')
-  ) {
-    console.log('received a comment');
-    return next();
   }
 
   return rewrite(new URL('https://github.com/apps/cr-gpt'));
