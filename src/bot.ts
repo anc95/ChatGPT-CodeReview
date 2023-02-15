e7a42ea50c9363f5482ee059f08f7ce8f14e2ccf
anc95 <1481988258@qq.com>
2023-02-15 16:08:33 +0800

    listen pullrequest open event

--- src/bot.ts
+++ src/bot.ts
@@ -2,6 +2,7 @@ import { Context, Probot } from 'probot';
 import { Chat } from './chat.js';
 
 const OPENAI_API_KEY = 'OPENAI_API_KEY';
+const MAX_PATCH_COUNT = 4000;
 
 export const robot = (app: Probot) => {
   const loadChat = async (context: Context) => {
@@ -26,6 +27,40 @@ export const robot = (app: Probot) => {
     const repo = context.repo();
     const chat = await loadChat(context);
 
-    const pl = context.payload.pull_request;
+    const {
+      data: { files },
+    } = await context.octokit.request(
+      `GET /repos/{owner}/{repo}/compare/{basehead}`,
+      {
+        owner: repo.owner,
+        repo: repo.repo,
+        basehead: `${context.payload.pull_request.base.sha}...${context.payload.pull_request.head.sha}`,
+      }
+    );
+
+    if (!files?.length) {
+      return;
+    }
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const patch = file.patch || '';
+
+      if (!patch || patch.length > MAX_PATCH_COUNT) {
+        continue;
+      }
+      const res = await chat?.codeReview(patch);
+
+      if (!!res) {
+        await context.octokit.pulls.createReviewComment({
+          repo: repo.repo,
+          owner: repo.owner,
+          pull_number: context.pullRequest().pull_number,
+          commit_id: file.sha,
+          path: file.filename,
+          body: res,
+        });
+      }
+    }
   });
 };
