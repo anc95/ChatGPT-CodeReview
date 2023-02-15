b5b5f955dcdfa92722c0711387d1db7b902a1637
小安 <anchao951220@outlook.com>
2023-02-15 18:15:45 +0800

    handle sync event (#21)
    * handle sync event
    
    * add code review keyword

--- src/bot.ts
+++ src/bot.ts
@@ -26,6 +26,11 @@ export const robot = (app: Probot) => {
   app.on(['pull_request.opened', 'pull_request.synchronize'], async (context) => {
     const repo = context.repo();
     const chat = await loadChat(context);
+    const pull_request = context.payload.pull_request;
+
+    if (pull_request.state === 'closed' || pull_request.locked || pull_request.draft) {
+      return;
+    }
 
     const data = await context.octokit.request(
       `GET /repos/{owner}/{repo}/compare/{basehead}`,
@@ -36,14 +41,31 @@ export const robot = (app: Probot) => {
       }
     );
 
-    const { files, commits } = data.data;
+    let { files: changedFiles, commits } = data.data;
 
-    if (!files?.length) {
+    if (context.payload.action === 'synchronize') {
+      if (commits.length >= 2) {
+        const { data: { files } } = await context.octokit.request(
+          `GET /repos/{owner}/{repo}/compare/{basehead}`,
+          {
+            owner: repo.owner,
+            repo: repo.repo,
+            basehead: `${commits[commits.length - 2].sha}...${commits[commits.length - 1].sha}`,
+          }
+        );
+
+        const filesNames = files?.map(file => file.filename) || [];
+        changedFiles = changedFiles?.filter(file => filesNames.includes(file.filename))
+      }
+    }
+
+
+    if (!changedFiles?.length) {
       return;
     }
 
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
+    for (let i = 0; i < changedFiles.length; i++) {
+      const file = changedFiles[i];
       const patch = file.patch || '';
 
       if (!patch || patch.length > MAX_PATCH_COUNT) {
