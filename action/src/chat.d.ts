bbb7f20cdf9a614c3a6e57db9163aab9bcdae16a
Seunghyun Kang <seunghyun.kang@omelet.ai>
2024-11-08 17:06:12 +0900

    run "npm run build"

--- action/src/chat.d.ts
+++ action/src/chat.d.ts
@@ -1,8 +1,6 @@
 export declare class Chat {
     private openai;
     private isAzure;
-    private apiVersion?;
-    private deployment?;
     constructor(apikey: string);
     private generatePrompt;
     codeReview: (patch: string) => Promise<string | null>;
