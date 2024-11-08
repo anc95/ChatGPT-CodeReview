12a11df6378f80cc1afcf2cb2b510c5ec812d980
anc95 <1481988258@qq.com>
2024-11-08 11:22:58 +0800

    build

--- action/src/chat.d.ts
+++ action/src/chat.d.ts
@@ -1,5 +1,8 @@
 export declare class Chat {
     private openai;
+    private isAzure;
+    private apiVersion?;
+    private deployment?;
     constructor(apikey: string);
     private generatePrompt;
     codeReview: (patch: string) => Promise<string | null>;
