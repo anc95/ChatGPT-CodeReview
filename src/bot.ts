75c8d14dc1dfb55d154202a4e3296f247a1efcca
anc95 <1481988258@qq.com>
2024-12-11 11:16:20 +0800

    fix: matching

--- src/bot.ts
+++ src/bot.ts
@@ -195,7 +195,7 @@ export const robot = (app: Probot) => {
 const matchPatterns = (patterns: string[], path: string) => {
   return patterns.some((pattern) => {
     try {
-      return minimatch(path, pattern)
+      return minimatch(path, pattern.startsWith('/') || pattern.startsWith('**') ? pattern : `**/${pattern}`);
     } catch {
       // if the pattern is not a valid glob pattern, try to match it as a regular expression
       try {
